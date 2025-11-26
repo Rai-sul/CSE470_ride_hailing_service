282c8a5c12d662c798e5af807961b6f6f67b645e
Waqar Wadud <nahiwaqar@gmail.com>
2025-11-27 00:47:52 +0600

    user login done

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,27 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const cors = require('cors');
 const express = require('express');
-const app = express()
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+
 connectToDb();
 
-
-app.use(cors()); // Accept req only for specific domain during production of !domain then not accept
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-app.get('/', (req,res)=>{
-    res.send("Hello World")
-})
+app.use(cookieParser());
 
 
 
 app.use('/users', userRoutes);
+
+
+
+
 
 module.exports = app;
