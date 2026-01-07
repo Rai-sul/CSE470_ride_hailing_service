a3f450cea0b34438ff9a1efc02e86f27ba70c8cd
Rai-sul <raisul.islam.mahi@gmail.com>
2026-01-07 12:32:47 +0600

    f_test

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -60,8 +60,6 @@ const UserSignup = () => {
     <div>
       <div className='p-7 h-screen flex flex-col justify-between'>
         <div>
-          <img className='w-16 mb-10' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s" alt="" />
-
           <form onSubmit={(e) => {
             submitHandler(e)
           }}>
